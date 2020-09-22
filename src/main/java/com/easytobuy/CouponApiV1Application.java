aa1c5f451a3d99e60a3deb253fa296099e8d5814
MaruthamSatish <smarutham@gmail.com>
2020-09-22 22:56:44 +0530

    Coupon API V1 First Commit

---
+++ src/main/java/com/easytobuy/CouponApiV1Application.java
@@ -0,0 +1,13 @@
+package com.easytobuy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CouponApiV1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CouponApiV1Application.class, args);
+	}
+
+}
---
+++ src/main/java/com/easytobuy/CouponRestController.java
@@ -0,0 +1,32 @@
+package com.easytobuy;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class CouponRestController {
+	@Autowired
+	private CouponRepository couponRepository;
+
+	@RequestMapping(value = "/coupon/{couponCode}", method = RequestMethod.GET)
+	public ResponseEntity<List<Coupon>> findByCouponCode(@PathVariable("couponCode") String couponCode) {
+		List<Coupon> getCoupons = couponRepository.findByCouponCode(couponCode);
+		if (getCoupons.isEmpty()) {
+			return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+		}
+		return new ResponseEntity<List<Coupon>>(getCoupons, HttpStatus.OK);
+	}
+	
+	public ResponseEntity<Coupon> getByCouponCode() {
+		return null;
+		
+	}
+}
