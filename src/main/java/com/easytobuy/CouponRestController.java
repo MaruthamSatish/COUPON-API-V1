80f96887b85f37f47a9e3663085586eb5a3ca776
MaruthamSatish <smarutham@gmail.com>
2020-09-24 15:10:34 +0530

    Delete,Save,Get Method Added to Coupon Service

--- src/main/java/com/easytobuy/CouponRestController.java
+++ src/main/java/com/easytobuy/CouponRestController.java
@@ -1,11 +1,18 @@
 package com.easytobuy;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,7 +23,7 @@ public class CouponRestController {
 	@Autowired
 	private CouponRepository couponRepository;
 
-	@RequestMapping(value = "/coupon/{couponCode}", method = RequestMethod.GET)
+	@RequestMapping(value = "/coupons/{couponCode}", method = RequestMethod.GET)
 	public ResponseEntity<List<Coupon>> findByCouponCode(@PathVariable("couponCode") String couponCode) {
 		List<Coupon> getCoupons = couponRepository.findByCouponCode(couponCode);
 		if (getCoupons.isEmpty()) {
@@ -25,9 +32,32 @@ public class CouponRestController {
 		}
 		return new ResponseEntity<List<Coupon>>(getCoupons, HttpStatus.OK);
 	}
-	
-	public ResponseEntity<Coupon> getByCouponCode() {
-		return null;
-		
+
+	@GetMapping("/coupons")
+	public ResponseEntity<List<Coupon>> getAllCoupons() {
+		List<Coupon> getAllCoupons = couponRepository.findAll().stream().collect(Collectors.toList());
+		return new ResponseEntity<List<Coupon>>(getAllCoupons, HttpStatus.OK);
+
+	}
+
+	@PostMapping("/coupons")
+	public ResponseEntity<Coupon> saveCoupon(@RequestBody Coupon coupons) {
+		try {
+			couponRepository.save(coupons);
+			return new ResponseEntity<Coupon>(coupons, HttpStatus.CREATED);
+		} catch (Exception e) {
+			return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
+
+	@DeleteMapping("/coupons/{couponId}")
+	public HttpStatus deleteCoupon(@PathVariable("couponId") Integer couponId) {
+		Optional<Coupon> coupon = couponRepository.findById(couponId);
+		if (coupon.isPresent()) {
+			couponRepository.deleteById(couponId);
+			return HttpStatus.NO_CONTENT;
+		} else {
+			return HttpStatus.NOT_FOUND;
+		}
 	}
 }
